609602818763600562ed6ae779e45d616b2de510
Tuur Dutoit <me@tuurdutoit.be>
2019-05-27 10:49:26 +0200

    Fix issue with Google Maps exceptions

--- engine/data/remote/routes/maps.py
+++ engine/data/remote/routes/maps.py
@@ -4,8 +4,29 @@ import os
 import logging
 
 logger = logging.getLogger(__name__)
-
 ADDRESS_FIELDS = ['street', 'city', 'state', 'postal_code', 'country']
+
+class GoogleMapsClient(googlemaps.Client):
+    def _get_body(self, response):
+        if response.status_code != 200:
+            raise googlemaps.exceptions.HTTPError(response.status_code)
+
+        body = response.json()
+
+        api_status = body["status"]
+        if api_status == "OK" or api_status == "ZERO_RESULTS":
+            return body
+
+        if api_status == "OVER_QUERY_LIMIT":
+            raise googlemaps.exceptions._OverQueryLimit(
+                api_status, body.get("error_message"))
+
+        raise GoogleMapsApiError(api_status, body, body.get("error_message"))
+
+class GoogleMapsApiError(googlemaps.exceptions.ApiError):
+    def __init__(self, status, body, message=None):
+        super(self, googlemaps.exceptions.ApiError).__init__(self, status, message)
+        self.body = body
 
 maps = googlemaps.Client(key=os.environ['MAPS_API_KEY'])
 
@@ -18,31 +39,21 @@ def distance_matrix(addresses, num_new_locations):
         for j in range(num_locations):
             # Don't calculate distance between same start/end locations or unchanged start/end locations
             if i != j and not (i >= num_new_locations and j >= num_new_locations):
-                directions = maps.directions(origin=addresses[i], destination=addresses[j])
-                
-                if directions and directions['status'] == 'OK':
+                try:
+                    directions = maps.directions(origin=addresses[i], destination=addresses[j])[0]
                     distances[i][j] = directions['routes'][0]['legs'][0]['duration']['value']
-                else:
-                    logger.debug('Maps error: %s | %s -> %s', directions['status'], addresses[i], addresses[j])
+                except Exception as e:
+                    logger.debug('Maps error: %s | %s -> %s', e, addresses[i], addresses[j])
                     distances[i][j] = None
-                    waypoints = directions.get('geocoded_waypoints', default=None)
                     
-                    if not waypoints or len(waypoints) < 2:
-                        valid[i] = False
-                        valid[j] = False
-                    else:
-                        if waypoints[0]['geocoder_status'] != 'OK':
-                            valid[i] = False
-                        if waypoints[1]['geocoder_status'] != 'OK':
-                            valid[j] = False
+                    if isinstance(e, GoogleMapsApiError):
+                        waypoints = e.body.get('geocoded_waypoints', default=None)
+                        
+                        if waypoints and len(waypoints) >= 2:
+                            if waypoints[0]['geocoder_status'] != 'OK':
+                                valid[i] = False
+                            if waypoints[1]['geocoder_status'] != 'OK':
+                                valid[j] = False
                     
     
     return distances, valid
-
-def geocode(address):
-    try:
-        res = maps.geocode(address)
-        location = res[0]['geometry']['location']
-        return (location['lat'], location['lng'])
-    except:
-        return None
